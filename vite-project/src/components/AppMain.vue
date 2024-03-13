005d9cca4f64ad9d5d823281ebec70434f2ef6cc
MironAlberto <alberto.miron19@gmail.com>
2024-03-13 18:08:16 +0100

    completed bonus

--- vite-project/src/components/AppMain.vue
+++ vite-project/src/components/AppMain.vue
@@ -5,19 +5,38 @@ import ProjectCard from './ProjectCard.vue';
 export default {
     data() {
         return {
-            projects: []
+            projects: [],
+            currentPage: 1,
+            lastPage: 1
         };
     },
     methods: {
-
-    },
-    created(){
-        axios.get('http://127.0.0.1:8000/api/projects')
+        getProjectsApi(page){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                    page: page
+                }
+            })
             .then(response => {
                 // console.log(response.data);
-
                 this.projects = response.data.results.projects.data;
+                this.currentPage = response.data.results.projects.current_page;
+                this.lastPage = response.data.results.projects.last_page;
             })
+        },
+        prevPage(){
+            if (this.currentPage > 1){
+                this.getProjectsApi(this.currentPage - 1);
+            }
+        },
+        nextPage(){
+            if (this.currentPage < this.lastPage){
+                this.getProjectsApi(this.currentPage + 1);
+            }
+        }
+    },
+    created(){
+        this.getProjectsApi(this.currentPage);
     },
     components: {
         ProjectCard
@@ -27,9 +46,26 @@ export default {
 
 <template>
     <main>
-        <h2 class="text-center my-2">
-            My Projects
-        </h2>
+        <div class="container mt-3">
+            <h5 class="d-flex justify-content-end fw-bolder mb-3">
+                Page {{ currentPage }} of {{ lastPage }}
+            </h5>
+            <div class="d-flex justify-content-between">
+                <button class="btn btn-dark text-dark" @click="prevPage()">
+                    <a href="#nogo" class="link-light text-decoration-none">
+                        Previous Page
+                    </a>
+                </button>
+                <button class="btn btn-dark" @click="nextPage()">
+                    <a href="#nogo" class="link-light text-decoration-none">
+                        Next Page
+                    </a>
+                </button>
+            </div>
+            <h2 class="text-center my-2">
+                My Projects
+            </h2>
+        </div>
 
         <ProjectCard v-for="project in projects" :key="project.id" :project="project"/>
     </main>
--- vite-project/src/components/ProjectCard.vue
+++ vite-project/src/components/ProjectCard.vue
@@ -21,6 +21,11 @@ export default {
                 Title: {{ project.title }}
             </div>
             <ul class="list-group list-group-flush">
+                <li class="list-group-item" v-if="project.cover_image != null">
+                    <div class="img-box">
+                        <img :src="'http://127.0.0.1:8000/storage/' + project.cover_image" :alt="project.title">
+                    </div>
+                </li>
                 <li class="list-group-item">
                     Type: {{ project.type.title }}
                 </li>
@@ -37,4 +42,14 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.img-box{
+    max-width: 200px;
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+
 </style>
